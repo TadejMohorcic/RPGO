43ae98aaf81394bd9fa4ecef79fef532c4944827
Tadej Mohorcic <tadej.mohorcic@gmail.com>
2022-12-06 14:21:29 +0100

    popravil prve vaje

--- Vaje 1/naloga2c.m
+++ Vaje 1/naloga2c.m
@@ -1,20 +1,20 @@
 % izbrane barve
-a = "#E9C46A";
-b = "#F4A261";
-c = "#E76F51";
+c1 = "#E9C46A";
+c2 = "#F4A261";
+c3 = "#E76F51";
 
 B = [0 0; 1 2; 3 3; 4 -1; 0 2];
 t = linspace(0,1);
 subplot(1,3,1);
-plotbezier(B,t,a);
+plotbezier(B,t,c1);
 
 % zrcaljenje krivulje čez y os
 Bz = B*[-1 0; 0 1];
 subplot(1,3,2);
-plotbezier(Bz,t,b);
+plotbezier(Bz,t,c2);
 
 % rotacija krivulje za 60 stopinj
 fi = pi/3;
 Br = B*[cos(fi) -sin(fi); sin(fi) cos(fi)]';
 subplot(1,3,3);
-plotbezier(Br,t,c);
+plotbezier(Br,t,c3);
